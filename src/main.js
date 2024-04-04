73101370995d4cdb537eae2e2ccbedc0bbecda21
FILIPI MARTINS <820057@EDUC.COLEGIOPOLITEC.COM.BR>
2024-04-04 08:38:52 -0300

    Pegando perguntas e adicionando-as à lista

--- src/main.js
+++ src/main.js
@@ -6,8 +6,19 @@
 
 
 const form = document.getElementById("adicionarPerguntaForm");
+const input_pergunta = document.getElementById("perguntaTextoInput");
+const lista_perguntas = document.getElementById("questionarioList");
 
 form.addEventListener('submit', (e) => {
     console.log("Formulário enviado!");
     e.preventDefault();
+
+    texto_pergunta = input_pergunta.value;
+    input_pergunta.value = "";
+    nova_pergunta = document.createElement("li");
+
+    nova_pergunta.innerText = texto_pergunta;
+
+    lista_perguntas.appendChild(nova_pergunta);
+
 })
