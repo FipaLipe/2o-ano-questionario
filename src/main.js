3fe48d7ca15fb4aeabae4ec1b0ac3c0bb2a51a5a
Filipi <filipi.martinss@hotmail.com>
2024-04-03 21:53:33 -0300

    Prevenindo comportamento padrão

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@
 // O evento de interesse para essa atividade é o "submit" do formulário.
 // Os elementos <input>  guardam o valor do texto digitado neles no atributo `.value`.
 // Lembre-se de que não queremos que o formulário seja enviado, para isso você deve usar o `e.preventDefault()`
-// Teste COMMIT
+
+
+const form = document.getElementById("adicionarPerguntaForm");
+
+form.addEventListener('submit', (e) => {
+    console.log("Formulário enviado!");
+    e.preventDefault();
+})
