30f5a8f7608838ad5a656f87bf91ccda8e073e6c
Filipi <filipi.martinss@hotmail.com>
2024-04-03 21:44:56 -0300

    teste primeiro commit

--- src/main.js
+++ src/main.js
@@ -3,3 +3,4 @@
 // O evento de interesse para essa atividade é o "submit" do formulário.
 // Os elementos <input>  guardam o valor do texto digitado neles no atributo `.value`.
 // Lembre-se de que não queremos que o formulário seja enviado, para isso você deve usar o `e.preventDefault()`
+// Teste COMMIT
